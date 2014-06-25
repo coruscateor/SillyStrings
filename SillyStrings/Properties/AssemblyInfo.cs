bfdc9732d93a8e1eab42df05d4785c34db862d01
Paul Saunders <SpaceCuration@gmail.com>
2014-06-25 15:20:33 +1200

    Updated AssemblyCopyrightAttribute declarations to include my name.

--- SillyStrings/Properties/AssemblyInfo.cs
+++ SillyStrings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SillyStrings")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Paul Saunders 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
